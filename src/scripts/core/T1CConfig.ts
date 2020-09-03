50243166d7f367c8b682e975ffd3466d66b72f30
Gilles Platteeuw <platteeuw.gilles@gmail.com>
2020-09-03 15:06:35 +0200

    remove ds url from config

--- src/scripts/core/T1CConfig.ts
+++ src/scripts/core/T1CConfig.ts
@@ -4,7 +4,6 @@ export class T1CConfigOptions {
     public t1cApiPort?: string,
     public t1cProxyUrl?: string,
     public t1cProxyPort?: string,
-    public dsUrl?: string,
     public jwt?: string
   ) {}
 }
@@ -13,7 +12,6 @@ export class T1CConfigOptions {
  * T1C SDK Configuration object.
  */
 export class T1CConfig {
-  private _dsUrl = 'https://acc-ds.t1t.io/';
   private _t1cApiUrl = 'https://t1c.t1t.io';
   private _t1cApiPort = '51983';
   private _t1cProxyUrl = 'https://t1c.t1t.io';
@@ -28,9 +26,6 @@ export class T1CConfig {
       }
       if (options.t1cApiPort) {
         this._t1cApiPort = options.t1cApiPort;
-      }
-      if (options.dsUrl) {
-        this._dsUrl = options.dsUrl;
       }
       if (options.t1cProxyUrl) {
         this._t1cProxyUrl = options.t1cProxyUrl;
@@ -54,14 +49,6 @@ export class T1CConfig {
 
   set t1cApiUrl(value: string) {
     this._t1cApiUrl = value;
-  }
-
-  get dsUrl(): string {
-    return this._dsUrl;
-  }
-
-  set dsUrl(value: string) {
-    this._dsUrl = value;
   }
 
   get t1cProxyUrl(): string {
